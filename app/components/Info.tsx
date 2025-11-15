57301e269adf322714fbbd0b017d0e85a25aa3fd
henryb21 <henryb0421@gmail.com>
2025-11-15 00:13:12 -0700

    Updated Info.tsx

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -2,11 +2,11 @@ export default function Info() {
   return (
     <div className="text-center mt-8">
       <p className="text-lg mb-2">
-        This is the second component for the assignment.
+        For my final project in web development I am interested in learning more about NextJS through the process of creating a workout tracking app.
       </p>
 
       <a
-        href="https://github.com/henryb21"
+        href="https://github.com/henryb21/CSCI331-Assignment-8"
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-500 underline hover:text-blue-700"
